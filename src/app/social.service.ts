2029c6b1e1ec5d2ba3599a5980a2be8f45b190b5
Ethan Beaver <ethanbeaver35@gmail.com>
2017-05-07 11:26:18 -0700

    WIP - Adding services

--- src/app/social.service.ts
+++ src/app/social.service.ts
@@ -5,16 +5,40 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SocialService {
   private s_url = 'http://localhost:8080';
+  private data: any;
   constructor(private http: Http) { }
 
   getAllPosts(): any {
-    this.http.get(this.s_url + 'api/userPost')
+    this.data = {'test': 'I hope you don\'t see this.'};
+    this.http.get(this.s_url + '/api/userPost')
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          this.data = data;
+        }
+      );
+    return this.data;
+  }
+
+  getPost(postID: string): any {
+    this.http.get(this.s_url + '/api/userPost/' + postID)
       .map(res => res.json())
       .subscribe(
         data => { return data; }
       );
   }
 
+  getUserPosts(userID: string): any {
+    this.http.get(this.s_url + '/api/userPost/user/' + userID)
+      .map(res => res.json())
+      .subscribe(
+        data => { return data; }
+      );
+  }
+
+  likePost(postID: string): void {
+
+  }
 
 
 }
