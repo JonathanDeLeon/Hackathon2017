b935d7b2cb954b37113b3b741739f3ea919719f8
Ethan Beaver <ethanbeaver35@gmail.com>
2017-05-07 14:05:30 -0700

    Lots more changes

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SocialService} from "../social.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-post',
@@ -6,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-
-  constructor() { }
+  newPost: any = {};
+  constructor(private ss: SocialService, private router: Router) { }
 
   ngOnInit() {
 
   }
 
+  createNewPost() {
+    const date = new Date();
+    this.newPost.date = (date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear();
+    this.newPost.like = 0;
+    this.newPost.userId = '3';
+    this.ss.createPost(this.newPost, (data) => {console.log(data); this.router.navigate(['/']);});
+  }
 }
